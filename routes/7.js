993f54b9cff67d7c7cdc679f3ed815a6a2396f26
ampersand <ampersand.dev@gmail.com>
2021-01-22 06:07:22 +0900

    auto commit

--- routes/7.js
+++ routes/7.js
@@ -1,63 +1,27 @@
-
-var path = require('path');
 var express = require('express');
 var router = express.Router();
 
 
-// http://localhost:3000/7/himawari
+
+// http://localhost:3000/7/azisai.jpg
 // http://localhost:3000/7/himawari.jpg
-// http://localhost:3000/images/dog.jpg
 
 
-
-
-/*
-// router.disable('etag');
-router.use(express.static('public/images/7', {
-    etag: false,
-    maxAge:31557600000,
- /*
+router.use(express.static('public/images/7/useCache', {
     setHeaders: (res, path, stat) => {
-        res.setHeader('Cache-Control', 'public, max-age=3600')
-        res.removeHeader('ETag');
-        res.header('Cache-Control', ['private', 'no-store', 'no-cache', 'must-revalidate', 'proxy-revalidate'].join(','));
-        res.header('no-cache', 'Set-Cookie');    },
-
-
-}))
- */
-
-
-router.use(express.static('public/images/7'));
-
-router.get('/himawari', function(req, res, next) {
-    console.log('7')
-    console.log(__dirname);
-    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.header("Pragma", "no-cache");
-    res.header("Expires", '100000000');
-    res.render('practice', { title: 'Practice',description: 'ここは練習ページです。'});
-
-});
-
-router.get('/azisai', function(req, res, next) {
-    console.log('7')
-    console.log(__dirname);
-    res.cacheControl = {
-        maxAge: 15552000,
-        public:true
-    };
-    res.send('<img src="azisai.jpg" alt="azisai">');
-
-});
-
-
-/*
-router.use(express.static('../public/images', {
-    setHeaders: function (res) {
-        res.cookie('3rdPartyCookie', 'bbb')
-    }
+        res.cacheControl = {
+            maxAge: 100,
+            public:true
+        };}
 }));
 
- */
+
+router.use(express.static('public/images/7/notUseCache', {
+    setHeaders: (res, path, stat) => {
+        res.cacheControl = {
+            noStore:true,
+            maxAge:0
+        };}
+}));
+
 module.exports = router;
